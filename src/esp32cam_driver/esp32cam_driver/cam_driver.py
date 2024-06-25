b4c70f8844fa159cc1a0422d1be676930b02b39d
Victor Cionca <victorcionca@gmail.com>
2024-06-25 09:04:39 +0100

    Publishing CameraInfo messages based on camera calibration

--- src/esp32cam_driver/esp32cam_driver/cam_driver.py
+++ src/esp32cam_driver/esp32cam_driver/cam_driver.py
@@ -3,6 +3,7 @@ from rclpy.executors import MultiThreadedExecutor
 from rclpy.node import Node
 from rclpy.callback_groups import MutuallyExclusiveCallbackGroup
 
+from sensor_msgs.msg import CameraInfo
 from sensor_msgs.msg import Image
 from example_interfaces.srv import Trigger
 
@@ -14,16 +15,33 @@ import requests
 import cv2
 from io import BytesIO
 import numpy as np
+from time import time_ns
 
 # TODO these could be node parameters
-stream_url = "http://10.0.0.2/stream"
-stream_ctrl_url = "http://10.0.0.2/control?var=stream&val={status}"
+cam_ip = "192.168.0.106"
+stream_url = "http://"+cam_ip+"/stream"
+stream_ctrl_url = "http://"+cam_ip+"/control?var=stream&val={status}"
+
+def create_caminfo():
+    """Create a caminfo for the espcam without a timestamp or sequence"""
+    caminfo = CameraInfo()
+    caminfo.width = 320
+    caminfo.height = 240
+    caminfo.distortion_model = "plumb_bob"
+    caminfo.d = [0.075469, -0.219867, -0.007941, 0.005980, 0.000000]
+    caminfo.k = [190.139395, 0.000000, 155.972114, 0.000000, 195.942656, 133.193820, 0.000000, 0.000000, 1.000000]
+    caminfo.p = [189.545212, 0.000000, 155.484702, 0.000000, 0.000000, 195.643066, 132.357145, 0.000000, 0.000000, 0.000000, 1.000000, 0.000000]
+    caminfo.binning_x = 1
+    caminfo.binning_y = 1
+    caminfo.header.frame_id = "camera_link"
+    return caminfo
 
 class Esp32CamDriver(Node):
     def __init__(self, args=None):
         super().__init__('esp32cam_driver')
         self.streaming = False
         self.img_publisher = self.create_publisher(Image, 'espcam/image', 1)
+        self.caminfo_publisher = self.create_publisher(CameraInfo, 'espcam/camera_info', 1)
         # Service to start streaming
         self.streaming_cbg = MutuallyExclusiveCallbackGroup()
         self.start_streaming_srv = self.create_service(Trigger,
@@ -106,9 +124,17 @@ class Esp32CamDriver(Node):
                     img_dat = BytesIO(image_data)
                     cv2_img = cv2.imdecode(np.frombuffer(img_dat.read(), np.uint8), 1)
                     # Convert the image to ros2 Image message
-                    img_msg = cv_br.cv2_to_imgmsg(cv2_img)
+                    img_msg = cv_br.cv2_to_imgmsg(cv2_img, encoding='rgb8')
+                    # Set a timestamp for the frame
+                    frame_timestamp_ns = time_ns()
                     # Publish the image
                     self.img_publisher.publish(img_msg)
+                    # Publish the camera info
+                    caminfo = create_caminfo()
+                    caminfo.header.stamp.sec = int(frame_timestamp_ns/1e9)
+                    caminfo.header.stamp.nanosec = int(frame_timestamp_ns%1e9)
+                    self.caminfo_publisher.publish(caminfo)
+                    self.get_logger().info(f"[{frame_timestamp_ns}] Generated frame")
                 except Exception as e:
                     self.get_logger().info(f"Failed to decode image: {e}")
         self.get_logger().info("Stopped streaming")
