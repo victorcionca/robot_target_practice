3231c1177b5d7d41be4013d5d4bf3880b88b9901
Victor Cionca <victorcionca@gmail.com>
2024-06-28 12:19:30 +0100

    Changed scanning to non-blocking

--- src/target_practice/target_practice/arm_controller.py
+++ src/target_practice/target_practice/arm_controller.py
@@ -8,8 +8,9 @@ sys.path.extend([
 
 from threading import Thread
 from interbotix_xs_modules.xs_robot.arm import InterbotixManipulatorXS
-from interbotix_moveit_interface_msgs.srv import MoveItPlan 
+from nav_msgs.srv import GetPlan
 from example_interfaces.srv import Trigger
+from rclpy.qos import QoSProfile, QoSHistoryPolicy
 
 class ArmController(InterbotixManipulatorXS):
 
@@ -21,54 +22,67 @@ class ArmController(InterbotixManipulatorXS):
         self.auto_finish = auto_finish
         self.robot_name = robot_name
         # Create move arm service, use node in super
-        self.move_arm_srv = self.core.create_service(MoveItPlan,
+        # Define queue depth 1 for the server
+        qos_profile = QoSProfile(
+            history=QoSHistoryPolicy.KEEP_LAST, depth=1
+        )
+        self.move_arm_srv = self.core.create_service(GetPlan,
                                                      f'/{robot_name}/move_arm',
-                                                     self.move_arm)
+                                                     qos_profile=qos_profile,
+                                                     callback=self.move_arm)
         self.sleep_arm_srv = self.core.create_service(Trigger,
                                                       f'/{robot_name}/sleep',
                                                       self.go_to_sleep)
+        self.scan_timer = None
+        self.target_scan_position = 0.785375
+        self.scan_time = 4.0 # seconds
         self.start_scan_srv = self.core.create_service(Trigger,
                                                  f'/{robot_name}/start_scan',
                                                  self.start_scan)
         self.stop_scan_srv = self.core.create_service(Trigger,
                                                  f'/{robot_name}/stop_scan',
                                                  self.stop_scan)
-        self.arm_scan_thread = None
+        self.is_scanning = False
         self.arm.set_ee_pose_components(x=0.15, y=0, z=0.24)
-        self.arm_scan_thread_running = False
         self.timer = None
 
     def start_scan(self, request, response):
-        self.arm_scan_thread_running = True
-        self.arm_scan_thread = Thread(target=self.arm_scan)
-        self.arm_scan_thread.start()
+        self.is_scanning = True
+        # Start immediately
+        self.scan_timer = self.core.create_timer(0.1, self.arm_scan)
         response.success = True
         return response
 
     def stop_scan(self, request, response):
-        self.arm_scan_thread_running = False
+        self.is_scanning = False
+        self.scan_timer.destroy()
+        # Uncomment the next line to bring back to start position
+        #self.arm.set_ee_pose_components(x=0.15, y=0, z=0.24)
         response.success = True
         return response
 
     def arm_scan(self):
-        self.core.get_logger().info('Start scanning')
-        while self.arm_scan_thread_running:
-            self.core.get_logger().info('Cycle')
-            self.arm.set_single_joint_position(f"waist", 0.785375, moving_time=4.0, blocking=True)
-            if not self.arm_scan_thread_running:
-                break
-            self.arm.set_single_joint_position(f"waist", -0.785375, moving_time=4.0, blocking=True)
-        self.core.get_logger().info('Done scanning')
-        self.arm.set_ee_pose_components(x=0.15, y=0, z=0.24)
+        if self.is_scanning == False: return
+        self.core.get_logger().info("Scanning")
+        self.arm.set_single_joint_position(f"waist",
+                                           self.target_scan_position,
+                                           moving_time=self.scan_time)
+        self.target_scan_position *= -1
+        self.scan_timer.destroy()
+        self.scan_timer = self.core.create_timer(self.scan_time, self.arm_scan)
         
     def move_arm(self, request, response):
-        target_point = request.ee_pose.position
+        target_point = request.start.point
+        duration_timestamp = request.start.header.stamp
+        duration = duration_timestamp.sec + duration_timestamp.nanosec/1e9
         self.core.get_logger().info(f'Moving arm to {target_point.x, target_point.y, target_point.z}')
         _, success = self.arm.set_ee_pose_components(x=target_point.x,
                                         y=target_point.y,
-                                        z=target_point.z)
-        if self.auto_finish:
-            self.timer = self.core.create_timer(1.0,self.finish)
+                                        z=target_point.z,
+                                        moving_time=duration,
+                                        blocking=False)
+        #if self.auto_finish:
+        #    self.timer = self.core.create_timer(1.0,self.finish)
         response.success = success
         return response
 
